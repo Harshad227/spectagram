c7ece5a3b74bba1cf8bfc672e0b24570c9774314
Harshad Kumar <81737776+Harshad227@users.noreply.github.com>
2022-06-06 17:31:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigator from './Navigation/DrawerNavigator';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+     <DrawerNavigator/>
+     </NavigationContainer>
+  );
+}
